4cf5dc7d0ae3759c7ff83f1c9fe36101cf56ff30
Divya <divya.amirtharaj@gmail.com>
2023-03-03 14:21:47 -0500

    adding logical clocks to queue

--- logical_clocks.py
+++ logical_clocks.py
@@ -4,18 +4,38 @@ import threading
 import queue
 from random import randint
 
+def gen_queues(machine_count):
+    system_queue = []
+    for i in range(machine_count):
+        system_queue.append(queue.Queue())
+    return system_queue
+        
+
 class VirtualMachine():
-    def __init__(self, ticks):
+    def __init__(self, ticks, machine_count, id):
+        self.machine_count = machine_count
         # set ID for the machine to be added to queue
         self.id = id
         # set a random clock rate (randint 1-6, number of ticks per real world seconds)
         self.ticks = ticks
-        # for holding incoming messages
-        self.queue = queue.Queue()
-        # init logs and clear logs each run
+        # dictionary of queues to hold messages for all virtual machines
+        self.system_queue = self.gen_queues()
+        # queue to hold incoming messages for this id
+        self.id_queue = self.system_queue[self.id]
+        # init logical clock
+        self.logical_clock = 0
+        # init logs and clear logs each run 
+    
+    def gen_queues(self):
+        system_queue = []
+        for i in range(self.machine_count):
+            system_queue.append(queue.Queue())
+        return system_queue
     
     def send_msg(self):
         global_time = time.time()
+        # increment logical clock by 1
+        self.logical_clock += 1
 
         if not self.queue.empty():
             print("Queue is not empty!")
@@ -24,12 +44,18 @@ class VirtualMachine():
             if val == 1:
                 # m0 sends to m1, m1 sends to m2, m2 sends to m0
                 rec = [(self.id + 1) % 3]
+                self.system_queue[rec].put(self.logical_clock)
+                # TODO: update log
             elif val == 2:
                 # m0 sends to m2, m1 sends to m0, m2 sends to m1
                 rec = [(self.id + 2) % 3]
+                self.system_queue[rec].put(self.logical_clock)
+                # TODO: update log
             elif val == 3:
                 # m0 sends to m1/2, m1 sends to m2/0, m2 sends to m0/1
                 rec = [(self.id + 1)%3, (self.id + 2)%3]
+                self.system_queue[rec].put(self.logical_clock)
+                # TODO: update log
             else:
                 pass
 
@@ -39,5 +65,6 @@ def run_server():
     pass 
 
 if __name__ == "__main__":
-    ports = [2048, 2049, 2050]
-    # create three VM/servers with each of the ports
+    vm = VirtualMachine(1, 3, 1)
+    #vm.print_vals()
+    vm.send_msg()
