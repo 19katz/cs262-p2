2189ccb62032d4ebb97bc4af1fd81232d86df1e4
Divya <divya.amirtharaj@gmail.com>
2023-03-03 13:31:45 -0500

    send ids

--- logical_clocks.py
+++ logical_clocks.py
@@ -1,16 +1,13 @@
 import multiprocessing
 import time
 import threading
-import socket
 import queue
 from random import randint
 
 class VirtualMachine():
-    def __init__(self, host, port, ticks):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((host, port))
-        self.host = host
-        self.port = port
+    def __init__(self, ticks):
+        # set ID for the machine to be added to queue
+        self.id = id
         # set a random clock rate (randint 1-6, number of ticks per real world seconds)
         self.ticks = ticks
         # for holding incoming messages
@@ -19,19 +16,20 @@ class VirtualMachine():
     
     def send_msg(self):
         global_time = time.time()
+
         if not self.queue.empty():
             print("Queue is not empty!")
         else:
             val = randint(1,10)
             if val == 1:
-                #send to machine 2
-                pass
+                # m0 sends to m1, m1 sends to m2, m2 sends to m0
+                rec = [(self.id + 1) % 3]
             elif val == 2:
-                #send to machine 3
-                pass
+                # m0 sends to m2, m1 sends to m0, m2 sends to m1
+                rec = [(self.id + 2) % 3]
             elif val == 3:
-                #send to machine 2 and 3
-                pass
+                # m0 sends to m1/2, m1 sends to m2/0, m2 sends to m0/1
+                rec = [(self.id + 1)%3, (self.id + 2)%3]
             else:
                 pass
 
