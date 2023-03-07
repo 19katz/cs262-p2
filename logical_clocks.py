89d6cd52b59ba90e83664b21a1fe85a2f903300e
19katz <19katherinez@students.harker.org>
2023-03-06 23:54:46 -0500

    theres a socket in my pocket

--- logical_clocks.py
+++ logical_clocks.py
@@ -9,7 +9,6 @@ from _thread import *
 from threading import Thread
 
 class VirtualMachine:
-    """Class that represents an individual machine."""
     def __init__(self, host, port, ticks, machine_count, id):
         self.machine_count = machine_count
         # host name for this machine
@@ -19,46 +18,47 @@ class VirtualMachine:
         # will store the socket that is used to connect to/with other machines
         self.socket = None
         # set ID for the machine to be added to queue
+        # probably don't need this?
         self.id = id
         # set a random clock rate (randint 1-6, number of ticks per real world seconds)
         self.ticks = ticks
         # dictionary of queues to hold messages for all virtual machines
-        self.system_queue = self.gen_queues()
+        # self.system_queue = self.gen_queues()
         # queue to hold incoming messages for this id
-        self.id_queue = self.system_queue[self.id]
+        self.id_queue = queue.Queue()
         # init logical clock
         self.logical_clock = 0
         # init logs and clear logs each run 
     
-    def consumer(conn):
+    def consumer(id, conn):
         print("Connection accepted: " + str(conn))
         while True:
             data = conn.recv(1024)
             data_msg = data.decode('ascii')
             print("Received message " + data_msg)
-            # add to queue 
-            # -- i'm not sure how the current queue system works?
-            # is it strictly necessary for every machine to hold every other machine's queue?
-    
-    def producer(other_port):
+            # add to queue
+            self.id_queue.put((data_msg))
+
+    def producer(ports_to_connect):
         success = False
+        socket = 
         while not success:
             try:
-                self.socket.connect((host, other_port))
-                print("Successfully connected to " + str(other_port))
+                for port in ports_to_connect:
+                    self.socket.connect((host, port))
+                    print("Successfully connected to " + str(port))
+                success = True
             except:
                 pass
         
         while True:
-            # based on the global variable val, send a message to this connection
-            # read message off queue here i guess?
-            # send_msg() 
+            # call send_msg on clock ticks, i think
     
-    def gen_queues(self):
-        system_queue = []
-        for i in range(self.machine_count):
-            system_queue.append(queue.Queue())
-        return system_queue
+    # def gen_queues(self):
+    #     system_queue = []
+    #     for i in range(self.machine_count):
+    #         system_queue.append(queue.Queue())
+    #     return system_queue
     
     def send_msg(self):
         global_time = time.time()
@@ -93,13 +93,20 @@ class VirtualMachine:
                 # TODO: update log with internal event, the system time, and the logical clock value.
                 pass
 
-    def start_machine():
+    def start_server():
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.host, self.port))
-        self.socket.lostin()
+        self.socket.listen()
         while True:
             conn, addr = self.socket.accept()
             start_new_thread(consumer, (conn,))
+
+    def machine(ports_to_connect):
+        init_thread = Thread(target=self.start_server, args=())
+        init_thread.start()
+        # delay to initialize server-side logic on all processes
+        time.sleep(5)
+        prod_thread = Thread(target=producer,args=(ports_to_connect))
 
 
 # will run each server -- loop and solicit messages, randomly
@@ -109,6 +116,23 @@ def run_server():
     pass 
 
 if __name__ == "__main__":
-    vm = VirtualMachine(1, 3, 1)
+    host = "localhost"
+    ports = [2048, 3048, 4048]
+    # still need to generate ticks randomly from 1-6
+    v1 = VirtualMachine(host, ports[0], ticks, 3, 1)
+    v2 = VirtualMachine(host, ports[1], ticks, 3, 2)
+    v2 = VirtualMachine(host, ports[2], ticks, 3, 3)
+
+    p1 = Process(target=v1.machine([3048, 4048]))
+    p2 = Process(target=v1.machine([2048, 4048]))
+    p3 = Process(target=v1.machine([2048, 3048]))
+
+    p1.start()
+    p2.start()
+    p3.start()
+
+    p1.join()
+    p2.join()
+    p3.join()
     #vm.print_vals()
-    vm.send_msg()
+    #vm.send_msg()
