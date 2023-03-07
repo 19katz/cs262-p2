a873366b059731bd0250727a22dc86834412b4ec
Divya <divya.amirtharaj@gmail.com>
2023-03-07 17:09:39 -0500

    yum

--- logical_clocks.py
+++ logical_clocks.py
@@ -4,13 +4,16 @@ import queue
 import os
 import socket
 from random import randint
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 from _thread import * 
 from threading import Thread
 from select import select
+from signal import signal, SIGPIPE, SIG_DFL
 
 global global_time
 global_time = time.time()
+
+signal(SIGPIPE,SIG_DFL)
 
 # logger
 def logging_util():
@@ -47,17 +50,8 @@ class SocketUtil(Process):
             sock.close()
         print("Servers closed")
 
-def connections(machine_count, id, host, port):
-    machine_connections = {}
-    for machine in range(machine_count - 1):
-        rec = (id + machine) % 3
-        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        conn.connect(host, port[rec])
-        machine_connections[rec] = conn
-    return machine_connections
-
 def send_msg(host, port, id, machine_count, run_time):
-    queue = queue.Queue()
+    queue = Manager().Queue()
     sock = SocketUtil(host, port[id], queue)
     sock.start()
 
@@ -65,7 +59,13 @@ def send_msg(host, port, id, machine_count, run_time):
     logical_clock = 0
 
     #logger
-    connections = connections(machine_count, id, host, port)
+    machine_connections = {}
+    for machine in range(machine_count - 1):
+        rec = (id + machine + 1) % 3
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.connect((host, port[rec]))
+        machine_connections[rec] = conn
+    
     for i in range(run_time):
         for j in range(ticks):
             logical_clock += 1
@@ -81,32 +81,45 @@ def send_msg(host, port, id, machine_count, run_time):
                 global val
                 val = randint(1,10)
                 if val == 1:
+                    print("val:", val)
                     # m0 sends to m1, m1 sends to m2, m2 sends to m0
-                    rec = [(id + 1) % 3]
-                    data = logical_clock.encode()
-                    connections[rec].send(data)
-                    print(data)
+                    rec = (id + 1) % 3
+                    print("id", id)
+                    print("receiver:", rec)
+                    print(machine_connections)
+                    data = str(logical_clock).encode('ascii')
+                    machine_connections[rec].send(data)
+                    print("sent ", data)
                     # TODO: update log with the send, the system time, and the logical clock time
                 elif val == 2:
+                    print("val:", val)
                     # m0 sends to m2, m1 sends to m0, m2 sends to m1
-                    rec = [(id + 2) % 3]
-                    data = logical_clock.encode()
-                    connections[rec].send(data)
-                    print(data)
+                    rec = (id + 2) % 3
+                    print("id", id)
+                    print("receiver:", rec)
+                    print(machine_connections)
+                    data = str(logical_clock).encode('ascii')
+                    machine_connections[rec].send(data)
+                    print("sent ", data)
                     # TODO: update log with the send, the system time, and the logical clock time
                 elif val == 3:
+                    print("val:", val)
                     # m0 sends to m1/2, m1 sends to m2/0, m2 sends to m0/1
                     rec = [(id + 1)%3, (id + 2)%3]
-                    data = logical_clock.encode()
-                    connections[rec].send(data)
-                    print(data)
+                    data = str(logical_clock).encode('ascii')
+                    for r in rec:
+                        print("id", id)
+                        print("receiver:", rec)
+                        print(machine_connections)
+                        machine_connections[r].send(data)
+                    print("sent ", data)
                     # TODO: update log with the send, the system time, and the logical clock time
                 else:
                     # TODO: update log with internal event, the system time, and the logical clock value.
                     pass
         time.sleep(1/ticks - (time.time() - start_time))
     
-    for connection in connections.values():
+    for connection in machine_connections.values():
         connection.close()
 
 if __name__ == "__main__":
@@ -115,10 +128,18 @@ if __name__ == "__main__":
     machine_count = 3
     global_time = time.time()
     run_time = 10
-    threads = []
 
-    for i in range(0, machine_count-1):
-        thread = Process(target=send_msg, args=(host, ports, i, machine_count, run_time))
-        thread.start()
-        threads.append(thread)
-        print("whoo")
+    thread1 = Process(target=send_msg, args=(host, ports, 0, machine_count, run_time))
+    thread2 = Process(target=send_msg, args=(host, ports, 1, machine_count, run_time))
+    thread3 = Process(target=send_msg, args=(host, ports, 2, machine_count, run_time))
+
+    thread1.start()
+    print("Started 1")
+    thread2.start()
+    print("Started 2")
+    thread3.start()
+    print("Started 3")
+
+    thread1.join()
+    thread2.join()
+    thread3.join()
